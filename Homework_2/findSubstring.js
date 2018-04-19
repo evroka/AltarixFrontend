0d0e893cc82a826c9e6b94ac2e790e0260b66412
evroka <nata_safonova@inbox.ru>
2018-04-19 22:28:47 +0400

    Удалены лишние файлы

--- Homework_2/findSubstring.js
+++
@@ -1,7 +0,0 @@
-function findStr(arr, str) {
-	let lowerArray = arr.map(elem => elem.toLowerCase());
-	let lowerStr = str.toLowerCase();
-	return lowerArray.some(elem => elem.includes(lowerStr));
-}
-
-
